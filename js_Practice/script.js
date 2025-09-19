cb71d979852bc0f07a9e28c50e70f774bc7f2d1c
sahildilshad <sahildilshad782@gmail.com>
2025-09-19 10:44:54 +0530

    Add files via upload

---
+++ js_Practice/script.js
@@ -0,0 +1,15 @@
+// let arr = ["a", "b", 23, "d", 8];
+// let res = arr.filter(value => typeof value == "number");
+// console.log(res);
+
+// destructring
+
+// let arr = [1, 23, 45, 32];
+// let [,value2,value3,value4] = arr;
+// console.log(value2)
+// const obj={
+//     name:"sahil",
+//     age:12
+// }
+// let {age,name}=obj
+// console.log(age,name);
